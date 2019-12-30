e7cbff20c4f30666b6e0d62f3fcd261c7fd2a304
AliAdilQureshi <shah87061@gmail.com>
2019-12-29 23:33:32 -0800

    almost complete frontend

---
+++ slick/index.php
@@ -0,0 +1,418 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link href="css/main.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/component.css" />
+    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  
+    
+    
+    
+    	<div class="container-fluid clpad2">
+        	<div class="col-lg-3 col-md-3 col-sm-3 tpl clpad2"><a href="#"><i class="fa fa-home"></i></a>
+            
+            
+            
+            </div>
+            
+            
+            <div class="col-lg-3 col-md-3 col-sm-3 tpr clpad2">
+            
+            <div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+            
+            </div>
+            
+            
+            <div class="col-lg-6 col-md-6 col-sm-6 tpr clpad2">
+            
+            <div id="sb-search" class="sb-search">
+            <form>
+            <input class="sb-search-input" placeholder="Enter your search term..." type="text" value="" name="search" id="search">
+            <input class="sb-search-submit" type="submit" value="">
+            <span class="sb-icon-search"><i class="fa fa-search"></i></span>
+            </form>
+            </div>         
+<!--<div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>-->            
+            
+            
+            
+            </div>
+            
+            
+            
+            
+            
+        </div>
+        
+        <div class="container-fluid clpad2">
+        	<div class="col-lg-3 col-md-3 col-sm-3 tpll clpad2"><a href="#"><img src="images/logo.png" alt="logo" class="img-responsive"></a></div>
+            <div class="col-lg-9 col-md-9 col-sm-9 tprr clpad2">
+            	<nav role="navigation" class="navbar navbar-inverse">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar" style="background:white;"></span>
+                                <span class="icon-bar" style="background:white;"></span>
+                                <span class="icon-bar" style="background:white;"></span>
+                            </button>
+                            <a href="#" style="font:normal 14px Arial; color:white;" class="navbar-brand visible-xs">Menu</a>
+                        </div>
+                        <!-- Collection of nav links, forms, and other content for toggling -->
+                        <div id="navbarCollapse" class="collapse navbar-collapse">
+                            <ul class="nav navbar-nav">
+                            
+                            <li><a title="About us" href="content.php?id=2">About us</a></li>
+                            <li><a title="Products" href="products.php?live=4_42_0_0">Products</a></li>
+                            <li><a title="Customer Services" href="content.php?id=3">Production</a></li>
+                            <li><a title="View Basket" href="basket.php">Quality Policy</a></li>
+                            <li><a title="Production Tour" href="tour.php">Downloads</a></li>
+                            <li><a title="Contact us" href="contact-us.php">View Inquiry</a></li>
+                            <li><a title="Contact us" href="contact-us.php">Contact us</a></li>
+                            </ul>
+                            
+                            
+                            
+                        </div>
+                    </nav>
+            </div>
+        </div>
+        
+        
+  <!--Banner Start-->
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<div class="carousel-inner">
+
+<div class="item active">
+<a href="#"><img src="images/banner.jpg" alt="" /></a>
+</div>
+
+<div class="item ">
+<a href="#"><img src="images/banner.jpg" alt="" /></a>
+</div>
+
+</div>
+<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+<span class="glyphicon glyphicon-chevron-left"></span>
+</a>
+<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+<span class="glyphicon glyphicon-chevron-right"></span>
+</a>
+</div>
+<!--Banner End-->       
+        
+    
+    	<div style="background:url(images/bg1.jpg) center center no-repeat; border-top:1px #fff solid;">
+        	<div class="container">
+            <div class="slidee1">
+            	<div class="col-lg-3 t2020">
+                	<a href="#">
+                	<div class="catss1">
+                    	<p>Hunting Caps</p>
+                    </div>
+                    </a>
+                    <img src="images/cat01.jpg" class="img-responsive center-block">
+                </div>
+                <div class="col-lg-3 t2020">
+                	<a href="#">
+                	<div class="catss1">
+                    	<p>Hunting Caps</p>
+                    </div>
+                    </a>
+                    <img src="images/cat01.jpg" class="img-responsive center-block">
+                </div>
+                <div class="col-lg-3 t2020">
+                	<a href="#">
+                	<div class="catss1">
+                    	<p>Hunting Caps</p>
+                    </div>
+                    </a>
+                    <img src="images/cat01.jpg" class="img-responsive center-block">
+                </div>
+                <div class="col-lg-3 t2020">
+                	<a href="#">
+                	<div class="catss1">
+                    	<p>Hunting Caps</p>
+                    </div>
+                    </a>
+                    <img src="images/cat01.jpg" class="img-responsive center-block">
+                </div>
+                <div class="col-lg-3 t2020">
+                	<a href="#">
+                	<div class="catss1">
+                    	<p>Hunting Caps</p>
+                    </div>
+                    </a>
+                    <img src="images/cat01.jpg" class="img-responsive center-block">
+                </div>
+            </div>    
+            </div>
+        </div>
+        
+        
+        <div class="container">
+        <div class="col-lg-4 t1515 txtbxx">
+        	<h2>What We Do</h2>
+            <img src="images/ss.jpg">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the ndustry's  Lorem Ipsum has been the industry's  rem Ipsum has been the industry's  the Lorem Ipsum rem</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since theLorem Ipsum has been the industry's  rem Ipsum has been the industry's </p>
+        </div>
+        <div class="col-lg-4 t1515 txtbxx">
+        	<h2>What We Do</h2>
+            <img src="images/ss.jpg">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the ndustry's  Lorem Ipsum has been the industry's  rem Ipsum has been the industry's  the Lorem Ipsum rem</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since theLorem Ipsum has been the industry's  rem Ipsum has been the industry's </p>
+        </div>
+        <div class="col-lg-4 t1515 txtbxx">
+        	<h2>What We Do</h2>
+            <img src="images/ss.jpg">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the ndustry's  Lorem Ipsum has been the industry's  rem Ipsum has been the industry's  the Lorem Ipsum rem</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since theLorem Ipsum has been the industry's  rem Ipsum has been the industry's </p>
+        </div>
+    	</div>
+        
+        
+        <div class="container">
+        	<div class="col-lg-6 t1515 newsbxx">
+            	<h2><span>News & Updates</span></h2>
+                
+                
+                <div class="row">
+                	<div class="col-lg-6 nnst t1010">
+                    	<span>19-05-2015</span>
+                    	<img src="images/new01.jpg" class="img-responsive center-block">
+                        <h3>New Products Launched</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ....</p>
+                    </div>
+                    <div class="col-lg-6 nnst t1010">
+                    	<span>19-05-2015</span>
+                    	<img src="images/new01.jpg" class="img-responsive center-block">
+                        <h3>New Products Launched</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard ....</p>
+                    </div>
+                </div>
+                
+                
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 t1515 productionbxx">
+            	<h2><span>Production Tour</span></h2>
+                <img src="images/production.jpg" class="img-responsive center-block">
+            </div>
+        </div>
+        
+    
+    <div style="background:#f4f4f4 url(images/ftrbg.png) top center no-repeat;">
+    	<div class="container">
+        	<div class="col-lg-12 ftrprdbx">
+            	<h2>Featured Products</h2>
+                
+                
+                <div class="col-lg-3 col-md-4 col-sm-6 t0025">
+                <div class="ftrprd">
+                	<a href="#">
+                	<div class="imgbx">
+                    <div class="ppg1">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    </div>
+                    <span class="helper"></span>
+                    <img src="images/eye.png" style="position:absolute; bottom:5px; left:5px;">
+                    <img class="resss" src="images/ssample.jpg">
+                    </div>
+                    </a>
+                    <div class="detbx">
+                    	<h3>Hunting Tool</h3>
+                        <h4>Art No: GIF-786</h4>
+                        <a class="addt" href="#">Add To Cart</a>
+                    </div>
+                
+                </div>
+              </div>
+              	<div class="col-lg-3 col-md-4 col-sm-6 t0025">
+                <div class="ftrprd">
+                	<a href="#">
+                	<div class="imgbx">
+                    <div class="ppg1">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    </div>
+                    <span class="helper"></span>
+                    <img src="images/eye.png" style="position:absolute; bottom:5px; left:5px;">
+                    <img class="resss" src="images/ssample.jpg">
+                    </div>
+                    </a>
+                    <div class="detbx">
+                    	<h3>Hunting Tool</h3>
+                        <h4>Art No: GIF-786</h4>
+                        <a class="addt" href="#">Add To Cart</a>
+                    </div>
+                
+                </div>
+              </div>
+              	<div class="col-lg-3 col-md-4 col-sm-6 t0025">
+                <div class="ftrprd">
+                	<a href="#">
+                	<div class="imgbx">
+                    <div class="ppg1">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    </div>
+                    <span class="helper"></span>
+                    <img src="images/eye.png" style="position:absolute; bottom:5px; left:5px;">
+                    <img class="resss" src="images/ssample.jpg">
+                    </div>
+                    </a>
+                    <div class="detbx">
+                    	<h3>Hunting Tool</h3>
+                        <h4>Art No: GIF-786</h4>
+                        <a class="addt" href="#">Add To Cart</a>
+                    </div>
+                
+                </div>
+              </div>
+              	<div class="col-lg-3 col-md-4 col-sm-6 t0025">
+                <div class="ftrprd">
+                	<a href="#">
+                	<div class="imgbx">
+                    <div class="ppg1">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    </div>
+                    <span class="helper"></span>
+                    <img src="images/eye.png" style="position:absolute; bottom:5px; left:5px;">
+                    <img class="resss" src="images/ssample.jpg">
+                    </div>
+                    </a>
+                    <div class="detbx">
+                    	<h3>Hunting Tool</h3>
+                        <h4>Art No: GIF-786</h4>
+                        <a class="addt" href="#">Add To Cart</a>
+                    </div>
+                
+                </div>
+              </div>
+                
+            </div>
+        </div>
+    </div>
+    
+    
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/classie.js"></script>
+	<script src="js/search.js"></script>
+    <script src="js/uisearch.js"></script>
+	<script>
+    new UISearch( document.getElementById( 'sb-search' ) );
+    </script>
+    
+    
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script type="text/javascript" src="slick/slick.min.js"></script>
+    
+    <script type="text/javascript">
+    $(document).ready(function(){
+			$('.slidee1').slick({
+			
+			infinite: true,
+			speed: 300,
+			slidesToShow: 4,
+			slidesToScroll: 4,
+			responsive: [
+			{
+			breakpoint: 1024,
+			settings: {
+			slidesToShow: 4,
+			slidesToScroll: 4,
+			infinite: true,
+			dots: false
+			}
+			},
+			{
+			breakpoint: 600,
+			settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+			}
+			},
+			{
+			breakpoint: 480,
+			settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1
+			}
+			}
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+			]
+			});
+			
+			
+			$('.slidee2').slick({
+			
+			infinite: true,
+			speed: 300,
+			slidesToShow: 4,
+			slidesToScroll: 4,
+			responsive: [
+			{
+			breakpoint: 1024,
+			settings: {
+			slidesToShow: 3,
+			slidesToScroll: 3,
+			infinite: true,
+			dots: false
+			}
+			},
+			{
+			breakpoint: 600,
+			settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+			}
+			},
+			{
+			breakpoint: 480,
+			settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1
+			}
+			}
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+			]
+			});
+	});
+  </script>
+    
+    
+    
+  </body>
+</html>
