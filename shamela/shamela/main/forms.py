e693ac49cdfe9db9fb17b0e31ad520406ddf1dc4
AliAdilQureshi <shah87061@gmail.com>
2020-02-13 14:30:57 -0800

    ..

--- shamela/shamela/main/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import UserBooks
-
-
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = UserBooks
-        fields = ('name', 'description', 'book_file')
